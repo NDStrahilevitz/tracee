cd13f2fb09c67ed4f9fd83795f54348c9ce1352b
Nadav Strahilevitz <nadav.strahilevitz@aquasec.com>
2022-02-13 14:25:47 +0000

    enable passing callback on bpf event receive

--- pkg/ebpf/tracee.go
+++ pkg/ebpf/tracee.go
@@ -63,6 +63,7 @@ type Config struct {
 	OSInfo             *helpers.OSInfo
 	Sockets            runtime.Sockets
 	ContainersEnrich   bool
+	OnEventRecv        func()
 }
 
 type CaptureConfig struct {
