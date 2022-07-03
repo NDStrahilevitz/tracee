492d1c8871467bc1767e0e15e87e77f271685df0
Nadav Strahilevitz <nadav.strahilevitz@aquasec.com>
2022-07-03 13:11:06 +0000

    types: add filters protocol

---
+++ types/protocol/filter.go
@@ -0,0 +1,143 @@
+package protocol
+
+import "reflect"
+
+type FilterOperator int
+
+const (
+	Equal FilterOperator = iota
+	NotEqual
+	Lower
+	LowerEqual
+	Greater
+	GreaterEqual
+)
+
+func (o FilterOperator) String() string {
+	switch o {
+	case Equal:
+		return "=="
+	case NotEqual:
+		return "!="
+	case Greater:
+		return ">"
+	case Lower:
+		return "<"
+	case GreaterEqual:
+		return ">="
+	case LowerEqual:
+		return "<="
+	}
+	return ""
+}
+
+func (o FilterOperator) not() FilterOperator {
+	switch o {
+	case Equal:
+		return NotEqual
+	case NotEqual:
+		return Equal
+	case Greater:
+		return LowerEqual
+	case Lower:
+		return GreaterEqual
+	case GreaterEqual:
+		return Lower
+	case LowerEqual:
+		return Greater
+	}
+	// sanity
+	return -1
+}
+
+type Filter struct {
+	Field    string
+	Operator FilterOperator
+	Value    []interface{}
+}
+
+// Not returns a "mirrored" filter with a negated operator
+func (f Filter) Not() Filter {
+	return Filter{f.Field, f.Operator.not(), f.Value}
+}
+
+//
+// filter building helpers
+//
+
+func EqualFilter(field string, vals ...interface{}) Filter {
+	return Filter{field, Equal, flattenIfaceArr(vals)}
+}
+
+func NotEqualFilter(field string, vals ...interface{}) Filter {
+	return Filter{field, NotEqual, flattenIfaceArr(vals)}
+}
+
+func LowerFilter(field string, vals ...interface{}) Filter {
+	return Filter{field, Lower, flattenIfaceArr(vals)}
+}
+
+func LessEqFilter(field string, vals ...interface{}) Filter {
+	return Filter{field, LowerEqual, flattenIfaceArr(vals)}
+}
+
+func GreaterFilter(field string, vals ...interface{}) Filter {
+	return Filter{field, Greater, flattenIfaceArr(vals)}
+}
+
+func GreatEqFilter(field string, vals ...interface{}) Filter {
+	return Filter{field, GreaterEqual, flattenIfaceArr(vals)}
+}
+
+//
+// string filter helpers
+//
+
+// PrefixFilter returns an EqualFilter with values representing prefixes
+func PrefixFilter(field string, prefixes ...string) Filter {
+	prefixVals := []interface{}{}
+	for _, prefix := range prefixes {
+		prefixVals = append(prefixVals, prefix+"*")
+	}
+	return Filter{field, Equal, prefixVals}
+}
+
+// SuffixFilter returns an EqualFilter with values representing suffixes
+func SuffixFilter(field string, suffixes ...string) Filter {
+	suffixVals := []interface{}{}
+	for _, suffix := range suffixes {
+		suffixVals = append(suffixVals, "*"+suffix)
+	}
+	return Filter{field, Equal, suffixVals}
+}
+
+// ContainsFilter returns an EqualFilter with values representing contained values
+func ContainsFilter(field string, contains ...string) Filter {
+	containedVals := []interface{}{}
+	for _, contained := range contains {
+		containedVals = append(containedVals, "*"+contained+"*")
+	}
+	return Filter{field, Equal, containedVals}
+}
+
+// flattenIfaceArr flattens an array of interface{} values
+// this is needed because we would like to pass []string values to the helpers
+func flattenIfaceArr(vals []interface{}) []interface{} {
+	res := make([]interface{}, 0)
+	for _, val := range vals {
+		if isSlice(val) {
+			sliceVal := reflect.ValueOf(val)
+			for i := 0; i < sliceVal.Len(); i++ {
+				res = append(res, sliceVal.Index(i).Interface())
+			}
+		} else {
+			res = append(res, val)
+		}
+	}
+	return res
+}
+
+func isSlice(v interface{}) bool {
+	kind := reflect.TypeOf(v).Kind()
+	return kind == reflect.Slice || kind == reflect.Array
+}
--- types/protocol/protocol_test.go
+++ types/protocol/protocol_test.go
@@ -22,3 +22,9 @@ func TestEvent_SetHeader(t *testing.T) {
 
 	assert.Equal(t, testHeaderInput, evt.Header(testHeader))
 }
+
+func TestFilter_FlattenIfaceSlice(t *testing.T) {
+	testSlice := []interface{}{[]string{"a", "b", "c"}, []int{1, 2, 3}, "bruh"}
+	expected := []interface{}{"a", "b", "c", 1, 2, 3, "bruh"}
+	assert.Equal(t, expected, flattenIfaceArr(testSlice))
+}
